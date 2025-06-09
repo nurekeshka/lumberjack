d0102195ab61d1516e78536c2dd2a51ccc3f033a
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2025-06-09 19:52:10 +0200

    feat: add initial project configuration

---
+++ apps/core/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+	@Inject(AppService)
+	private readonly service: AppService;
+
+	@Get()
+	getHello(): string {
+		return this.service.getHello();
+	}
+}
