5de3b6a39ba67c45c5c9f062e10eefdf832a3ef5
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2025-06-09 21:24:05 +0200

    feat: add recent changes

--- apps/core/vitest.config.ts
+++ apps/core/vitest.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  appType: 'custom',
-  test: {
-    name: 'core',
-    globals: true,
-    environment: 'node',
-    include: ['./**/*.spec.ts'],
-    exclude: ['./node_modules/**', './dist/**'],
-  },
+	appType: 'custom',
+	test: {
+		name: 'core',
+		globals: true,
+		environment: 'node',
+		include: ['./**/*.spec.ts'],
+		exclude: ['./node_modules/**', './dist/**'],
+	},
 });
