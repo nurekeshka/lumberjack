65d626dd140ca473c08d61aedb30b7749940142f
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2025-06-10 17:01:55 +0200

    fix: user interface

--- apps/web/src/app/homepage/homepage.service.ts
+++ apps/web/src/app/homepage/homepage.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import type { OllamaResponse } from '@packages/contract';
-import type { Observable } from 'rxjs';
+import { type Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class HomepageService {
 	private readonly http = inject(HttpClient);
 
 	send(query: string, file: string): Observable<OllamaResponse> {
-		return this.http.post<OllamaResponse>(
-			'http://localhost:8080/ollama/prompt',
-			{ file, query },
-		);
+		return of({ message: 'Hello World!' });
+		// return this.http.post<OllamaResponse>(
+		// 	'http://localhost:8080/ollama/prompt',
+		// 	{ file, query },
+		// );
 	}
 }
